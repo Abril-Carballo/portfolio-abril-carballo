d26dccf147956574d40896016d45e8d3a4bf5fc6
Abril-Carballo <abril.carballo12@gmail.com>
2025-08-11 18:54:59 -0300

    Agregue scroll con js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Seleccionamos todos los botones dentro del nav
 const botones = document.querySelectorAll('nav button');
 
+
 botones.forEach(boton => { // forEach loop
   // Agregamos transición suave
   boton.style.transition = 'transform 0.2s ease'; // ease --> suave
@@ -24,4 +25,17 @@ botones.forEach(boton => { // forEach loop
   // Evento hover (cuando se pasa el mouse por arriba)
   boton.addEventListener('mouseover', agrandar);
   boton.addEventListener('mouseout', restaurar);
+});
+
+
+
+botones.forEach(boton => {
+  boton.addEventListener('click', () => {
+    const targetID = boton.getAttribute('data-target');
+    const targetSection = document.getElementById(targetID);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
 });
